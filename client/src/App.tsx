42758abb058d95577b75e9b2da678b4a76da1035
contatoyvesx <contato.yvesx@gmail.com>
2025-11-28 16:29:34 -0300

    Remove scheduling page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,11 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-import Agendar from "./pages/Agendar";
 
 function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
-      <Route path={"/agendar"} component={Agendar} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
