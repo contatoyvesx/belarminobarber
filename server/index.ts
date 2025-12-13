d1eec145d66dec0b621d8a931ee5dbea81efc898
contatoyvesx <contato.yvesx@gmail.com>
2025-12-13 09:09:53 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { registrarRotasDeAgenda } from "./schedule";
 import { supabase } from "./supabase";
+import { registrarRotasAdmin } from "./admin";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
